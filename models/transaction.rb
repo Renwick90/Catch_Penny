701576a323da5c363d3924256c3a419e34b8c834
renwick drysdale <renwick.drysdale@gmail.com>
2016-07-04 09:27:07 +0100

    initial commit

---
+++ models/transaction.rb
@@ -0,0 +1,19 @@
+require('pg')
+require_relative('../db/sql_runner')
+require_relative('teg')
+require_relative('merchant')
+
+class Rental
+
+  attr_reader(:id, :merchant_id, :tag_id, :amount, :date, :description, :type)
+
+  def initialize( options )
+    @id = options['id'].to_i
+    @merchant_id = options['book_id'].to_i
+    @tag_id = options['member_id'].to_i
+    @amount = options['amount'].to_i
+    @date = options['date']
+ 
+  end
+
+end
